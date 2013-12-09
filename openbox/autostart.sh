81c52f22e187883dbcc9ae146ede016cd78323e8
anton-trapeznikov <anton.trapeznikov@gmail.com>
2013-12-09 12:06:09 +0600

    09.12.13

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,5 +1,12 @@
+eval $(gnome-keyring-daemon) &
+xcompmgr -c &
 tilda -h &
 feh --bg-scale /usr/share/pixmaps/debi.jpg &
 tint2 &
-volumeicon &
+volti &
 gxneur &
+skype &
+cairo-dock &
+rc-update add wicd default &
+thunar --daemon &
+clipit &
